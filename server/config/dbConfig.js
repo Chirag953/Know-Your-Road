944d150a48bc6eafa234dee01bc85921afd6e58d
chirag <chiragdutta953@gmail.com>
2025-08-06 00:20:48 +0530

    Initial commit - KnowYourRoad project setup

---
+++ server/config/dbConfig.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URL);
+
+const connection = mongoose.connection;
+ 
+connection.on("connected", () => {
+  console.log("MongoDB connection established successfully");
+});
+connection.on("error", (err) => {
+  console.error(`MongoDB connection error: ${err}`);
+});
+
+module.exports = connection;
---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const dbConfig = require('./config/dbConfig');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});  
